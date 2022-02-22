985715efb1a5e03ddf986ceea2921b2adee2b9d1
huijung <huijung01@naver.com>
2022-02-22 15:21:16 +0900

    로그인 ajax 로직구현

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -29,6 +29,26 @@ let index = {
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		}); 
+	},
+	
+	login: function(){
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+		};
+		
+		$.ajax({ 
+			type: "POST",
+			url: "/blog/api/user/login",
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // body데이터가 어떤 차입인디(MIME)
+			dataType: "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면)=> javascript오브젝트로 변경
+		}).done(function(resp){
+			alert("로그인이 완료되었습니다.");
+			location.href="/blog";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
 	}
 }
 
